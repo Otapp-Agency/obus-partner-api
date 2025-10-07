a405486829e2df64ba22738546ad4c9623161969
Godfrey <godfrey.desidery@otapp.net>
2025-10-07 11:33:00 +0300

    refactor: remove unused parameterless methods from bus and seat map services

--- src/main/java/com/obuspartners/modules/partner_integration/bmslg/agent_v8/buses/BmsLgBusService.java
+++ src/main/java/com/obuspartners/modules/partner_integration/bmslg/agent_v8/buses/BmsLgBusService.java
@@ -3,6 +3,5 @@ package com.obuspartners.modules.partner_integration.bmslg.agent_v8.buses;
 import com.obuspartners.modules.partner_integration.bmslg.agent_v8.dto.BmsLgSearchBusRequestDto;
 
 public interface BmsLgBusService {
-    public Object searchBuses();
     public Object searchBuses(BmsLgSearchBusRequestDto requestDto);
 }
